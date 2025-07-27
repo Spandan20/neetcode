b08cb88d9d06a2eac17d4e3dfa3df731d639e35a
Spandan20 <spandanbusiness@outlook.com>
2025-07-27 21:47:18 +0530

    Added Contains Duplicate

--- src/main/java/Arrays/ContainsDuplicate.java
+++ src/main/java/Arrays/ContainsDuplicate.java
@@ -1,4 +1,23 @@
 package Arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,3};
+        boolean result = hasDuplicate(nums);
+        System.out.println(result);
+    }
+
+    private static boolean hasDuplicate(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+            if (freq.get(num) > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
